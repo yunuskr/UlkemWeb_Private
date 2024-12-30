6f8c0bca871ba65b84a25327f482b0263cd38814
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2024-12-30 20:11:43 +0300

    Admin layout eklendi.

--- UlkemWebApp/Controllers/AdminController.cs
+++ UlkemWebApp/Controllers/AdminController.cs
@@ -18,14 +18,18 @@ public class AdminController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+    public IActionResult CicekListesi()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Siparisler()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        return View();
+    }   
 }
